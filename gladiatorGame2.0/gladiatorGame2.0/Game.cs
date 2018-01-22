6391493265605cd20d71b1f454a3e986889ae054
ben.skp <benjaminsandland@gmail.com>
2018-01-22 10:23:22 +0100

    added 'menu' + 'game' class - cleaned up in program.cs

---
+++ gladiatorGame2.0/gladiatorGame2.0/Game.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gladiatorGame2._0
+{
+    class Game
+    {
+        Menu menu = new Menu();
+        public static List<Enemy> enemies = new List<Enemy>();
+        public static Character hero;
+        public static Weapon weapon = new Weapon();
+        public static Armor armor = new Armor();
+        public static Enemy opponent = new Enemy();
+
+        public void Combat()
+        {
+            int enemyID = 0; // Enemy count, will increment if user wins the round
+            int round = 1; /* Increments after every turn of you and opponent deals damage
+                              - to prevent initializing same  opponent over & over */
+
+            #region Add Enemies
+            //AddEnemies
+            Weapon peasantWeapon = new Weapon("N/A", 2, 7);
+            Armor peasantArmor = new Armor(15, 5);
+            enemies.Add(new Enemy("Pete the Peasant", 65, 10, peasantWeapon, peasantArmor));
+
+            Weapon knightWeapon = new Weapon("N/A", 5, 9);
+            Armor knightArmor = new Armor(20, 0);
+            enemies.Add(new Enemy("Kyle the Knight", 75, 5, knightWeapon, knightArmor));
+
+            Weapon gladiatorWeapon = new Weapon("N/A", 6, 11);
+            Armor gladiatorArmor = new Armor(20, 5);
+            enemies.Add(new Enemy("Gary the Gladiator", 80, 2));
+            #endregion Add Enemies
+
+
+            while (hero.isAlive && (enemies.Count > enemyID))
+            {
+                hero.currentHealth = hero.health;
+                menu.Print("This is round " + round + " in the arena." + "\n" + "Good luck!", ConsoleColor.Cyan);
+                opponent = enemies[enemyID];
+
+                while (hero.isAlive && opponent.isAlive)
+                {
+                    Console.Clear();
+                    // Actual combat-system
+                    // TODO: Remove decimal numbers after damage dealt by enemy (currently is rounded down, but on output-text)
+                    int dmg = hero.weapon.RollWeaponDmg();
+                    opponent.health -= dmg;
+                    menu.Print("You're in combat with a " + opponent.name + "!", ConsoleColor.Red);
+                    menu.Print("\n" + "You dealt " + dmg + " points of damage!", ConsoleColor.Green);
+                    menu.Print("Your opponents health is now: " + opponent.health, ConsoleColor.Yellow);
+
+                    Random defRoll = new Random();
+                    int defensiveRoll = defRoll.Next(0, 100);
+
+                    // Calculate chance to dodge and block
+                    if (defensiveRoll < hero.agility + hero.blockChance)
+                    {
+                        menu.Print("\n" + "You dodged your opponents damage!", ConsoleColor.Green);
+                    }
+
+                    // Calculate damage reduction from armor
+                    else
+                    {
+                        double calculatedDmg = opponent.GetEnemyDmg() * (1 - (armor.dmgReduc / 100d));
+                        hero.currentHealth -= (int)Math.Floor(calculatedDmg);
+                        menu.Print("\n" + opponent.name + " dealt " + calculatedDmg + " points of damage!", ConsoleColor.Red);
+                    }
+
+                    if (!opponent.isAlive)
+                    {
+                        menu.Print("You are the victor! Congratulations, you are a true gladiator!", ConsoleColor.Yellow);
+                        menu.Print("You'll be moving on to the next round!", ConsoleColor.Yellow);
+                        enemyID++;
+                        Console.ReadKey();
+                        continue;
+                    }
+
+                    if (!hero.isAlive)
+                    {
+                        menu.Print("You have been slain!", ConsoleColor.Red);
+                        Console.ReadKey();
+                        continue;
+                    }
+                    menu.Print("Your healthpoints are now at: " + hero.currentHealth + "!", ConsoleColor.Yellow);
+
+                    Console.ReadKey();
+                    Console.Clear();
+                }
+                round++; // Increments after every round
+            }
+        }
+    }
+}
---
+++ gladiatorGame2.0/gladiatorGame2.0/Menu.cs
@@ -0,0 +1,152 @@
+﻿using System;
+
+namespace gladiatorGame2._0
+{
+    class Menu
+    {
+        public static Character hero;
+        public static string name;
+        public static Light lightChar = new Light(name);
+        public static Medium medChar = new Medium(name);
+        public static Heavy heavyChar = new Heavy(name);
+
+        public void Print(string msg, ConsoleColor color = ConsoleColor.Gray)
+        {
+            Console.ForegroundColor = color;
+            Console.WriteLine(msg);
+            Console.ResetColor();
+        }
+
+        public void Title(string title)
+        {
+            Print("********" + title + "********");
+        }
+
+        public string Request(string paramater)
+        {
+            Console.Write(paramater);
+            string input = Console.ReadLine();
+            return input;
+        }
+
+        public void Greet()
+        {
+            Print("Welcome, to the Grand Arena!", ConsoleColor.Green);
+            Print("In here, you will fight for your life and glory!", ConsoleColor.Green);
+            Print("If you succeed, you will receive large sums of gold and a chance to become the next master of your own team of gladiators! \n", ConsoleColor.Green);
+            Print("Press any key to continue..", ConsoleColor.Red);
+            Console.ReadKey();
+        }
+
+        public string NameChar()
+        {
+            while (true)
+            {
+                Console.Clear();
+                Print("Gladiator, what is your name?", ConsoleColor.Yellow);
+                string name = Request("Name: ");
+
+                if (name != "")
+                {
+                    Print("Alright, good luck in the arena " + name + "!", ConsoleColor.Green);
+                    Console.ReadKey();
+                    return name;
+                }
+
+                // Basic error-handling - if above if-statement fails, string must be empty
+                else
+                {
+                    Print("Please enter a name!", ConsoleColor.Red);
+                    Console.ReadKey();
+                }
+            }
+
+        }
+
+        public int ChooseChar()
+        {
+            int choice = 0;
+            Console.Clear();
+            Print("Before you enter the arena, you must chose your weapons of armor", ConsoleColor.Yellow);
+            Console.ReadKey();
+
+            Console.Clear();
+            Title("CHOOSE YOUR PATH");
+            Print("You have three options: \n", ConsoleColor.Yellow);
+            Print("1) Light armor, one handed sword and a shield \n", ConsoleColor.Red);
+            Print("2) Medium armor, with a large spear \n", ConsoleColor.Red);
+            Print("3) Heavy armor, with a big two-handed axe \n", ConsoleColor.Red);
+            choice = int.Parse(Console.ReadLine());
+
+            switch (choice)
+            {
+                case 1:
+                    Console.Clear();
+                    Print("You have chosen the light armor with a one-handed sword and shield! \n", ConsoleColor.Green);
+                    Print("Press any key to continue..", ConsoleColor.Red);
+                    Console.ReadKey();
+                    hero = lightChar;
+                    break;
+
+                case 2:
+                    Console.Clear();
+                    Print("You have chosen the medium armor with a spear! \n", ConsoleColor.Green);
+                    Print("Press any key to continue..", ConsoleColor.Red);
+                    Console.ReadKey();
+                    Medium medChar = new Medium(name);
+                    hero = medChar;
+                    break;
+
+                case 3:
+                    Console.Clear();
+                    Print("You have chosen the heavy armor with the two-handed axe! \n", ConsoleColor.Green);
+                    Print("Press any key to continue..", ConsoleColor.Red);
+                    Console.ReadKey();
+                    Heavy heavyChar = new Heavy(name);
+                    hero = heavyChar;
+                    break;
+            }
+            return choice;
+        }
+
+        public void DisplayStats()
+        {
+            // TODO: Add the option to have the option displayed - currently is displayed every time
+            Console.Clear();
+            Title("CHARACTER SHEET");
+            Print("Here's your current items: \n", ConsoleColor.Green);
+            for (int i = 0; i < hero.items.Length; i++)
+            {
+                string item = hero.items[i];
+                // Put in 'N/A' text if off-hand is null
+                if (hero.items[2] == null && i == 2)
+                {
+                    Print(hero.itemLabels[i] + "N/A", ConsoleColor.Yellow);
+                }
+                else
+                {
+                    Print(hero.itemLabels[i] + item + "\n", ConsoleColor.Yellow);
+                }
+            }
+
+            Print("\n" + "Here's your stats: \n", ConsoleColor.Green);
+            for (int i = 0; i < hero.stats.Length; i++)
+            {
+                int stats = hero.stats[i];
+
+                // Put in 'N/A' text if block-chance is 0 - due to having two-hander equipped
+                if (hero.stats[3] == 0 && i == 3)
+                {
+                    Print(hero.statLabels[i] + "N/A", ConsoleColor.Yellow);
+                }
+                else
+                {
+                    Print(hero.statLabels[i] + stats, ConsoleColor.Yellow);
+                }
+
+            }
+            Print("\n" + "Press any key to continue..", ConsoleColor.Red);
+            Console.ReadKey();
+        }
+    }
+}
